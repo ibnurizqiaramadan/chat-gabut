fa14b55ddfdcea68994c3c0dc0ec9f66f4793cc5
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-04-21 17:42:20 +0700

    create template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import React from 'react';
+import Navbar from '@/components/navbar';
+import Sidebar from '@/components/sidebar';
 
 const inter = Inter({ subsets: [ 'latin' ] });
 
@@ -17,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>
+          <Navbar />
+          <Sidebar />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
