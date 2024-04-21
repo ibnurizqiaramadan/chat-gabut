eceb4f3363b6e359075598ee4ccfb60e37bb53fb
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-04-21 18:59:10 +0700

    Add next ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: [
@@ -15,6 +16,25 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [ nextui({
+    prefix: 'nextui', // prefix for themes variables
+    addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+    defaultTheme: 'dark', // default theme from the themes object
+    defaultExtendTheme: 'dark', // default theme to extend on custom themes
+    layout: {}, // common layout tokens (applied to all themes)
+    themes: {
+      light: {
+        layout: {}, // light theme layout tokens
+        colors: {}, // light theme colors
+      },
+      dark: {
+        layout: {}, // dark theme layout tokens
+        colors: {
+          // background: '#1E293B',
+        }, // dark theme colors
+      },
+      // ... custom themes
+    },
+  }) ],
 };
 export default config;
