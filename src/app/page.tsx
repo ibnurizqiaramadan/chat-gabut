247bd8cbc871742928dc7968a7f08a16eb4e88e7
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-04-23 22:49:41 +0700

    Implement chat store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
+'use client';
+
 import ChatContainer from '@/components/chat/chatContainer';
 import ChatInput from '@/components/chat/chatInput';
-import { Chats } from '@/helpers/types';
+import { ChatStore } from '@/helpers/types';
+import { useEffect } from 'react';
+import { chatStore } from '@/store/chatStore';
 
 export default function Home() {
-  const data: Chats = [
-    {
-      uuid: '1',
-      text: 'Hello',
-      sender: true,
-      timestamp: +new Date(),
-    },
-    {
-      uuid: '2',
-      text: 'How are you?',
-      sender: false,
-      timestamp: +new Date(),
-    },
-  ];
+  const data = chatStore((state: ChatStore) => state.chats);
+  const addChat = chatStore((state: ChatStore) => state.addChat);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      addChat({
+        uuid: String(+new Date()),
+        text: `Hi ${data.length + 1} ${Math.random()}`,
+        sender: Number((Math.random() * 100).toFixed(0)) % 2 === 0 ? true : false,
+        timestamp: +new Date(),
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [ addChat, data.length ]);
+
   return (
     <div className="maincontent flex flex-col">
       <ChatContainer data={data} />
