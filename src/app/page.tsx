a2f36a229bcad24f2da6f43eb3ac6d34b7f46144
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-04-28 00:04:23 +0700

    Implement new store and component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,39 @@
 
 import ChatContainer from '@/components/chat/chatContainer';
 import ChatInput from '@/components/chat/chatInput';
-import { ChatStore } from '@/helpers/types';
+import { ChatListStore, ChatStore } from '@/helpers/types';
 import { useEffect, useCallback } from 'react';
 import { chatStore } from '@/store/chatStore';
-import { sendChat } from '@/server/chatServer';
+import { sendChat, createNewChat } from '@/server/chatServer';
+import { chatListStore } from '@/store/chatListStore';
 
 export default function Home() {
-  const data = chatStore((state: ChatStore) => state.chats);
-  const addChat = chatStore((state: ChatStore) => state.addChat);
+  const { chats, addChat, clearChats } = chatStore((state: ChatStore) => state);
+  const { createChat, clearChatlist } = chatListStore<ChatListStore>((state) => state);
 
   const sendChatToServer = useCallback(async () => {
     const data = await sendChat();
     addChat(data);
   }, [ addChat ]);
 
+  const addUser = useCallback(async () => {
+    const data = await createNewChat();
+    createChat(data);
+  }, [ createChat ]);
+
   useEffect(() => {
     const timer = setInterval(() => {
       sendChatToServer();
+      addUser();
+      chats.length >= 100 && (clearChats(), clearChatlist());
     }, 1000);
 
     return () => clearInterval(timer);
-  }, [ sendChatToServer ]);
+  }, [ clearChats, chats.length, sendChatToServer, createChat, clearChatlist, addUser ]);
 
   return (
     <div className="maincontent flex flex-col">
-      <ChatContainer data={data} />
+      <ChatContainer data={chats} />
       <ChatInput />
     </div>
   );
