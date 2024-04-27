c01d3881e6bc7d8607558e91f10dd09b6b076c11
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-04-28 00:02:06 +0700

    Create type for chatlist

--- src/helpers/types.d.ts
+++ src/helpers/types.d.ts
@@ -12,3 +12,20 @@ export type ChatStore = {
   addChat: (newChat: Chat) => void
   clearChats: () => void
 }
+
+export type User = {
+  uuid: string
+  username: string
+  name: string
+  avatar: string
+  isOnline: boolean
+}
+
+export type Users = User[]
+
+export type ChatListStore = {
+  chatList: Users
+  createChat: (user: User) => void
+  removeChat: (uuid: string) => void
+  clearChatlist: () => void
+}
