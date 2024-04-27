3cfaf3bed23db1eb4e8ad2e33524eadf75bb240c
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-04-28 00:03:55 +0700

    Create server component for createNewChat

--- src/server/chatServer.ts
+++ src/server/chatServer.ts
@@ -1,4 +1,4 @@
-import { Chat } from '@/helpers/types';
+import { Chat, User } from '@/helpers/types';
 import { v4 as uuid } from 'uuid';
 
 export const sendChat = (): Promise<Chat> => {
@@ -8,6 +8,18 @@ export const sendChat = (): Promise<Chat> => {
       text: `Hello ${Math.random()} ${uuid()}`,
       sender: Number((Math.random() * 100).toFixed(0)) % 2 === 0 ? true : false,
       timestamp: Date.now(),
+    });
+  });
+};
+
+export const createNewChat = (): Promise<User> => {
+  return new Promise((resolve) => {
+    resolve({
+      avatar: 'https://flowbite.com/docs/images/logo.svg',
+      name: uuid(),
+      uuid: uuid(),
+      isOnline: true,
+      username: uuid(),
     });
   });
 };
