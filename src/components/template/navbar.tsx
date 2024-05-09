29ab0490fdf16f955a44b692880ea83f385de57f
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-09 22:24:47 +0700

    Using next image

--- src/components/template/navbar.tsx
+++ src/components/template/navbar.tsx
@@ -1,7 +1,11 @@
+'use client';
+
+import Image from 'next/image';
+
 export default function Navbar() {
   return (
     <div className="navbar bg-white text-black p-4 content-center flex items-center gap-4">
-      <img src="https://flowbite.com/docs/images/logo.svg" alt="" />
+      <Image src="https://flowbite.com/docs/images/logo.svg" alt="" width={32} height={32} />
       <h1>Topan Ndut</h1>
     </div>
   );
