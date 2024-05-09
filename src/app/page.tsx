0239a305224144ffb971c37f98cb21714bb5321e
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-09 22:23:50 +0700

    Remove test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,13 @@
+/* eslint-disable no-unused-vars */
 'use client';
 
 import ChatContainer from '@/components/chat/chatContainer';
 import ChatInput from '@/components/chat/chatInput';
-import { ChatListStore, ChatStore } from '@/helpers/types';
-import { useEffect, useCallback } from 'react';
+import { ChatStore } from '@/helpers/types';
 import { chatStore } from '@/store/chatStore';
-import { sendChat, createNewChat } from '@/server/chatServer';
-import { chatListStore } from '@/store/chatListStore';
 
 export default function Home() {
-  const { chats, addChat, clearChats } = chatStore((state: ChatStore) => state);
-  const { createChat, clearChatlist } = chatListStore<ChatListStore>((state) => state);
-
-  const sendChatToServer = useCallback(async () => {
-    const data = await sendChat();
-    addChat(data);
-  }, [ addChat ]);
-
-  const addUser = useCallback(async () => {
-    const data = await createNewChat();
-    createChat(data);
-  }, [ createChat ]);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      sendChatToServer();
-      addUser();
-      chats.length >= 100 && (clearChats(), clearChatlist());
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, [ clearChats, chats.length, sendChatToServer, createChat, clearChatlist, addUser ]);
+  const { chats } = chatStore((state: ChatStore) => state);
 
   return (
     <div className="maincontent flex flex-col">
