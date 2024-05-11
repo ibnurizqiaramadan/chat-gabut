4776fb97fdfa0f4208a762b86424e1bbe0564539
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-12 00:13:59 +0700

    Refactor code and add comment

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,24 +5,37 @@ import React, { useEffect } from 'react';
 import { User } from '@/helpers/types';
 import { createID } from '@/helpers/funtions';
 import { userStore } from '@/store';
+import { userServer } from '@/server';
 
+/**
+ * A React component that provides the necessary context for the application.
+ *
+ * @param {Object} props - The props object.
+ * @param {React.ReactNode} props.children - The child components to be rendered.
+ * @return {JSX.Element} The JSX element representing the Providers component.
+ */
 export function Providers({ children }: { children: React.ReactNode }) {
   const { setUser } = userStore((state) => state);
 
-  useEffect(() => {
-    const user: User = localStorage.getItem('user') as unknown as User;
-    if (user) return setUser(JSON.parse(user as unknown as string));
-    const name = prompt('Enter Name');
-    const newUser: User = {
-      avatar: 'https://flowbite.com/docs/images/logo.svg',
-      name: name?.trim() == '' ? createID() : name?.trim() as string,
-      uuid: createID(),
-      isOnline: true,
-      username: createID(),
+  useEffect( () => {
+    /**
+     * Retrieves the user from the server based on the stored ID in local storage.
+     * If the user is found, it sets the user in the state.
+     * If the user is not found, it prompts the user to enter a name and creates a new user with the provided name.
+     * The new user's ID is stored in local storage and set in the state.
+     *
+     * @return {Promise<void>} A promise that resolves when the user is set in the state.
+     */
+    const getUser = async (): Promise<void> => {
+      const id = localStorage.getItem('uid');
+      const user = await userServer.getUser(id as string) as User;
+      if (id) return setUser(user);
+      const name = prompt('Enter Name');
+      const newUser = await userServer.createNewUser(name?.trim() || createID());
+      localStorage.setItem('uid', newUser.id);
+      setUser(newUser);
     };
-    localStorage.setItem('user', JSON.stringify(newUser));
-    setUser(newUser);
-    console.log('new user created');
+    getUser();
   }, [ setUser ]);
 
   return (
