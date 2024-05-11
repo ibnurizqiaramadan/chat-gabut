709026be8cc67e2bd6a981dfb965d4b27e693f5f
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-12 00:14:44 +0700

    Display name (temp)

--- src/components/template/sidebar.tsx
+++ src/components/template/sidebar.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import ChatList from '@/components/chat/chatList';
+import { userStore } from '@/store';
 
 export default function Sidebar() {
+  const user = userStore((state) => state.user);
+
   return (
     <aside className="sidebar bg-white text-black" aria-label="Sidebar">
       <div className='p-5 sidenav'>
-        <p className="text-2xl font-bold">Chats</p>
+        <p className="text-2xl font-bold">Chats - {user?.name}</p>
       </div>
       <hr className=""/>
       <div className='sidecontent overflow-auto'>
