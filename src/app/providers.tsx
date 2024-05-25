f81592d29a3cec5c4efed991bf8883f76da38e8a
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-25 22:00:45 +0700

    Implement socket

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,8 +4,9 @@ import { NextUIProvider } from '@nextui-org/react';
 import React, { useEffect } from 'react';
 import { User } from '@/helpers/types';
 import { createID } from '@/helpers/funtions';
-import { userStore } from '@/store';
+import { userStore, chatListStore, appStore } from '@/store';
 import { userServer } from '@/server';
+import { io } from 'socket.io-client';
 
 /**
  * A React component that provides the necessary context for the application.
@@ -17,6 +18,19 @@ import { userServer } from '@/server';
 export function Providers({ children }: { children: React.ReactNode }) {
   const { setUser } = userStore((state) => state);
 
+  // eslint-disable-next-line no-unused-vars
+
+
+  const connectSocket = () => {
+    const newSocket = io('http://localhost:3004');
+    appStore.getState().setSocket(newSocket);
+    const socket = appStore.getState().appState.socket;
+
+    socket?.on('connect', () => {
+      console.log('Connected to socket');
+    });
+  };
+
   useEffect( () => {
     /**
      * Retrieves the user from the server based on the stored ID in local storage.
@@ -26,10 +40,18 @@ export function Providers({ children }: { children: React.ReactNode }) {
      *
      * @return {Promise<void>} A promise that resolves when the user is set in the state.
      */
+    const getChatList = () => {
+      const chatList = localStorage.getItem('chatList');
+      if (chatList) {
+        chatListStore.setState({ chatList: JSON.parse(chatList) });
+      }
+      connectSocket();
+    };
     const getUser = async (): Promise<void> => {
       const id = localStorage.getItem('uid');
       const user = await userServer.getUser(id as string) as User;
-      if (id) return setUser(user);
+      appStore.getState().setUserId(id as string);
+      if (id) return (setUser(user), getChatList());
       const name = prompt('Enter Name');
       const newUser = await userServer.createNewUser(name?.trim() || createID());
       localStorage.setItem('uid', newUser.id);
