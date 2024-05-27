3eba1337e795ed526ba13ed69b17f6aac320bdec
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-27 09:02:30 +0700

    Update chatStore for multiple user chat

--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -1,11 +1,49 @@
 import { create } from 'zustand';
-import { Chat, ChatStore } from '@/helpers/types';
+import { Chat, ChatStore, User } from '@/helpers/types';
 
 export const chatStore = create<ChatStore>((set) => ({
-  chats: [ ],
-  addChat: (newChat: Chat) => set((state) => ({ chats: [ ...state.chats, newChat ] })),
-  clearChats: () => set(() => ({ chats: [] })),
-  changePending: (chatData: Chat) => set((state) => ({
-    chats: state.chats.map((chat) => (chat.id === chatData.id ? { ...chat, isPending: false, timestamp: chatData.timestamp } : chat)),
-  })),
+  chatsData: [],
+  addChat: (user: User, newChat: Chat) => {
+    set((state) => {
+      const userIndex = state.chatsData.findIndex((data) => data.user.id === user.id);
+      if (userIndex === -1) {
+        return {
+          chatsData: [ ...state.chatsData, { user, chats: [ newChat ] } ],
+        };
+      }
+      return {
+        chatsData: state.chatsData.map((data, index) =>
+            index === userIndex ? { ...data, chats: [ ...data.chats, newChat ] } : data
+        ),
+      };
+    });
+    localStorage.setItem('chats', JSON.stringify(chatStore.getState().chatsData));
+  },
+  clearChats: (user: User) =>
+    set((state) => ({
+      chatsData: state.chatsData.map((data) =>
+        data.user.id === user.id ? { ...data, chats: [] } : data
+      ),
+    })),
+  changePending: (user: User, chatData: Chat) => {
+    set((state) => ({
+      chatsData: state.chatsData.map((data) =>
+        data.user.id === user.id ?
+          {
+            ...data,
+            chats: data.chats.map((chat) =>
+                chat.id === chatData.id ? { ...chat, isPending: false } : chat
+            ),
+          } :
+          data
+      ),
+    }));
+    localStorage.setItem('chats', JSON.stringify(chatStore.getState().chatsData));
+  },
+  loadChats: () => {
+    const chats = localStorage.getItem('chats');
+    if (chats) {
+      set({ chatsData: JSON.parse(chats) });
+    }
+  },
 }));
