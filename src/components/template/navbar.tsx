f5330f390d74490cc9478675d9c309128881812f
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-27 09:00:10 +0700

    Using open chat name insted of Topan Ndut

--- src/components/template/navbar.tsx
+++ src/components/template/navbar.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import Image from 'next/image';
+import { appStore } from '@/store';
 
 export default function Navbar() {
+  const { appState } = appStore((state) => state);
   return (
     <div className="navbar bg-white text-black p-4 content-center flex items-center gap-4">
       <Image src="https://flowbite.com/docs/images/logo.svg" alt="" width={32} height={32} />
-      <h1>Topan Ndut</h1>
+      <h1>{appState.openedChat?.name ?? 'Select Chat'}</h1>
     </div>
   );
 }
