63e7ff873ad9ad2e66f3d0cd66d2e0510185e292
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-27 21:12:42 +0700

    Update chatList - get last message - get last time message - implement moment

--- src/components/chat/chatList.tsx
+++ src/components/chat/chatList.tsx
@@ -1,13 +1,31 @@
 'use client';
 
 import Image from 'next/image';
-import { chatListStore, appStore } from '@/store';
+import { chatListStore, appStore, chatStore } from '@/store';
 import { ChatListStore } from '@/helpers/types';
+import moment from 'moment';
 
 export default function ChatList() {
   // eslint-disable-next-line no-unused-vars
   const { chatList } = chatListStore<ChatListStore>((state) => state);
   const { appState, setOpenedChat } = appStore((state) => state);
+  const { chatsData } = chatStore((state) => state);
+
+  const getLastMessage = (userId: string) => {
+    const data = chatsData.filter((data) => data.user?.id === userId)[0]?.chats ?? [];
+    return data[data.length - 1];
+  };
+
+  const getLastTimeMessage = (userId: string) => {
+    const data = chatsData.filter((data) => data.user?.id === userId)[0]?.chats ?? [];
+    if (data[data.length - 1]?.timestamp == undefined) return '';
+    const time = moment(data[data.length - 1]?.timestamp);
+    if (time == undefined) return '';
+    const now = moment();
+    if (time.isSame(now, 'day')) return time.format('HH:mm');
+    if (time.isSame(moment().subtract(1, 'days'), 'day')) return 'yesterday';
+    return time.format('YYYY-MM-DD');
+  };
 
   return (
     <div className='flex flex-col gap-1 px-2 py-3'>
@@ -21,9 +39,13 @@ export default function ChatList() {
           }}
         >
           <Image src={user.avatar} alt="" width={32} height={32} className='rounded-full' />
-          <div className='flex flex-col'>
+          <div className='flex flex-col flex-grow ml-2'>
             <p className='font-bold'>{user.name} {user.isOnline && <span className='text-green-500'>Online</span>}</p>
-            <p className='overflow-hidden'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit beatae repudiandae quibusdam veniam eius consequuntur, quam provident ullam sunt debitis omnis ex saepe necessitatibus, sequi qui, dolores deleniti! Dolorum, molestias!</p>
+            <p className='overflow-hidden'>{getLastMessage(user.id)?.text ?? ''}</p>
+          </div>
+          <div className='flex flex-col items-end ml-2'>
+            <p className='font-bold'>{getLastTimeMessage(user.id)}</p>
+            <p className=''></p>
           </div>
         </div>
       )}
