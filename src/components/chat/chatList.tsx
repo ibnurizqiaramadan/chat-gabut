69d1326c469654e4e998967c05b89222ab55325c
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-27 08:59:12 +0700

    Show 'No Chats' when user don't have chat open

--- src/components/chat/chatList.tsx
+++ src/components/chat/chatList.tsx
@@ -1,20 +1,28 @@
 'use client';
 
 import Image from 'next/image';
-import { chatListStore } from '@/store';
+import { chatListStore, appStore } from '@/store';
 import { ChatListStore } from '@/helpers/types';
 
 export default function ChatList() {
   // eslint-disable-next-line no-unused-vars
   const { chatList } = chatListStore<ChatListStore>((state) => state);
+  const { appState, setOpenedChat } = appStore((state) => state);
 
   return (
-    <div className='flex flex-col gap-4 p-4'>
+    <div className='flex flex-col gap-1 px-2 py-3'>
+      {chatList.length === 0 && <p>No Chats</p>}
       {chatList.map((user) =>
-        <div key={user.id} className="flex gap-2 bg-slate-600 rounded-xl p-2 h-[4rem] select-none">
+        <div
+          key={user.id}
+          className={`flex gap-2 ${appState.openedChat?.id == user.id ? 'bg-slate-700 hover:bg-slate-600' : ''} rounded-xl p-2 h-[4rem] select-none cursor-pointer hover:bg-slate-800`}
+          onClick={() => {
+            setOpenedChat(user);
+          }}
+        >
           <Image src={user.avatar} alt="" width={32} height={32} className='rounded-full' />
           <div className='flex flex-col'>
-            <p className='font-bold'>{user.name}</p>
+            <p className='font-bold'>{user.name} {user.isOnline && <span className='text-green-500'>Online</span>}</p>
             <p className='overflow-hidden'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit beatae repudiandae quibusdam veniam eius consequuntur, quam provident ullam sunt debitis omnis ex saepe necessitatibus, sequi qui, dolores deleniti! Dolorum, molestias!</p>
           </div>
         </div>
