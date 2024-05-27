6b018cf6d35b5394f160325ef9a62ea17ae18a29
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-27 08:56:27 +0700

    Filter chat data by selected user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@
 import ChatContainer from '@/components/chat/chatContainer';
 import ChatInput from '@/components/chat/chatInput';
 import { ChatStore } from '@/helpers/types';
-import { chatStore } from '@/store/chatStore';
+import { chatStore, appStore } from '@/store';
 
 export default function Home() {
-  const { chats } = chatStore((state: ChatStore) => state);
+  const { chatsData } = chatStore((state: ChatStore) => state);
+  const { appState } = appStore((state) => state);
+
+  const data = chatsData.filter((data) => data.user?.id === appState.openedChat?.id)[0]?.chats ?? [];
 
   return (
     <div className="maincontent flex flex-col">
-      <ChatContainer data={chats} />
+      <ChatContainer data={data} />
       <ChatInput />
     </div>
   );
