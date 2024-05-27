66a3db62d70a7e4a9e9032154b901948caee6e33
ibnurizqiarmadan <dadanibnu61@gmail.com>
2024-05-27 09:02:07 +0700

    Update createChat on chat list store

--- src/store/chatListStore.ts
+++ src/store/chatListStore.ts
@@ -4,11 +4,18 @@ import { ChatListStore, User } from '@/helpers/types';
 export const chatListStore = create<ChatListStore>((set) => ({
   chatList: [ ],
   user: null as User | null,
-  createChat: (user: User) => set((state) => (
-    { chatList:
-      [ ...state.chatList, user ],
-    })
-  ),
+  createChat: (user: User) => {
+    const userExist = chatListStore.getState().chatList.find((chat) => chat.id === user.id);
+    if (userExist) return;
+    set((state) => ({
+      chatList: [
+        ...state.chatList,
+        user,
+      ],
+    }));
+    localStorage.setItem('chatList', JSON.stringify(chatListStore.getState().chatList));
+  },
+
   removeChat: (id: string) => set((state) => ({ chatList: state.chatList.filter((chat) => chat.id !== id) })),
   clearChatlist: () => set(() => ({ chatList: [] })),
   checkUserExist: (userId: string) => set((state) => (
